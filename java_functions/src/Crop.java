31e054aa9e4bd2dbfdffdd26c4badce6b9005ac4
Louis Lomboy <llomboy@uw.edu>
2024-11-09 13:04:13 -0800

    Creatinga Crop.java class

---
+++ java_functions/src/Crop.java
@@ -0,0 +1,13 @@
+import java.awt.image.BufferedImage;
+import java.awt.Graphics2D;
+
+public class Crop {
+
+    /**
+     * Crops the given image
+     * @author Louis Lomboy
+     */
+    public cropImage() {
+
+    }
+}
