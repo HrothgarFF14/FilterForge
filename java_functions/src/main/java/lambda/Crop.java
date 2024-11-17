a5495c61be71d53c14c7a8a6d285ee8af32e40cd
Louis Lomboy <llomboy@uw.edu>
2024-11-16 23:44:00 -0800

    implement crop.java

--- java_functions/src/main/java/lambda/Crop.java
+++ java_functions/src/main/java/lambda/Crop.java
@@ -1,25 +1,103 @@
-package main.java.lambda;
+package lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import saaf.Inspector;
+import saaf.Response;
 
 import java.awt.image.BufferedImage;
-import java.awt.Graphics2D;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.Base64;
+import java.util.HashMap;
+import javax.imageio.ImageIO;
 
-/**
- * This class is used to crop an image to a specified size.
- */
-public class Crop {
+public class Crop implements RequestHandler<Request, HashMap<String, Object>> {
+
     /**
-     * This method crops an image to a specified size.
-     * @param image The image to crop.
-     * @param width The width to crop the image to.
-     * @param height The height to crop the image to.
-     * @return The cropped image.
+     * Lambda Function Handler
+     *
+     * @param theRequest Request POJO with defined variables from Request.java
+     * @param theContext
+     * @return HashMap that Lambda will automatically convert into JSON.
      */
-    public static BufferedImage crop(BufferedImage image, int width, int height) {
-        BufferedImage croppedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g2d = croppedImage.createGraphics();
-        g2d.drawImage(image, 0, 0, width, height, 0, 0, width, height, null);
-        g2d.dispose();
-        return croppedImage;
+    public HashMap<String, Object> handleRequest(Request theRequest, Context theContext) {
+
+        // Collect initial data.
+        Inspector inspector = new Inspector();
+        inspector.inspectAll();
+
+        //****************START FUNCTION IMPLEMENTATION*************************
+        HashMap<String, Object> responseMap = new HashMap<>();
+        try {
+            // Decode the base64 image
+            BufferedImage image = base64ToBufferedImage(theRequest.getImage());
+
+            // Validate crop dimensions
+            if (!validateCropDimensions(image, theRequest.getX(), theRequest.getY(), theRequest.getWidth(), theRequest.getHeight())) {
+                throw new IllegalArgumentException("Invalid crop dimensions");
+            }
+
+            // Crop the image
+            BufferedImage croppedImage = image.getSubimage(theRequest.getX(), theRequest.getY(), theRequest.getWidth(), theRequest.getHeight());
+            BufferedImage copyOfCroppedImage = new BufferedImage(croppedImage.getWidth(), croppedImage.getHeight(), image.getType());
+            copyOfCroppedImage.createGraphics().drawImage(croppedImage, 0, 0, null);
+
+            // Encode the cropped image to base64
+            String base64CroppedImage = bufferedImageToBase64(copyOfCroppedImage);
+
+            // Add the cropped image to the response
+            responseMap.put("croppedImage", base64CroppedImage);
+        } catch (Exception e) {
+            inspector.addAttribute("error", e.getMessage());
+        }
+
+        //****************END FUNCTION IMPLEMENTATION***************************
+
+        // Collect final information such as total runtime and CPU deltas.
+        inspector.inspectAllDeltas();
+        responseMap.putAll(inspector.finish());
+        return responseMap;
     }
-    
+
+    /**
+     * Validates the crop dimensions to ensure they are within the bounds of the source image.
+     *
+     * @param image the source image
+     * @param x the x coordinate of the upper-left corner of the specified rectangular region
+     * @param y the y coordinate of the upper-left corner of the specified rectangular region
+     * @param width the width of the specified rectangular region
+     * @param height the height of the specified rectangular region
+     * @return true if the dimensions are valid, false otherwise
+     */
+    private boolean validateCropDimensions(BufferedImage image, int x, int y, int width, int height) {
+        return x >= 0 && y >= 0 && width > 0 && height > 0 && x + width <= image.getWidth() && y + height <= image.getHeight();
+    }
+
+    /**
+     * Converts a BufferedImage to a base64 string.
+     *
+     * @param image the BufferedImage to convert
+     * @return the base64 string representation of the image
+     * @throws Exception if an error occurs during encoding
+     */
+    private String bufferedImageToBase64(BufferedImage image) throws Exception {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ImageIO.write(image, "png", bos);
+        byte[] imageBytes = bos.toByteArray();
+        return Base64.getEncoder().encodeToString(imageBytes);
+    }
+
+    /**
+     * Converts a base64 string to a BufferedImage.
+     *
+     * @param base64Image the base64 string to convert
+     * @return the BufferedImage
+     * @throws Exception if an error occurs during decoding
+     */
+    private BufferedImage base64ToBufferedImage(String base64Image) throws Exception {
+        byte[] imageBytes = Base64.getDecoder().decode(base64Image);
+        ByteArrayInputStream bis = new ByteArrayInputStream(imageBytes);
+        return ImageIO.read(bis);
+    }
 }
--- java_functions/src/main/java/lambda/Request.java
+++ java_functions/src/main/java/lambda/Request.java
@@ -1,17 +1,21 @@
 package lambda;
 
 /**
- *
- * @author Wes Lloyd
+ * Request class for handling input parameters.
  */
 public class Request {
 
-    String name;
+    private String name;
+    private String image;
+    private int x;
+    private int y;
+    private int width;
+    private int height;
 
     public String getName() {
         return name;
     }
-    
+
     public String getNameALLCAPS() {
         return name.toUpperCase();
     }
@@ -20,11 +24,55 @@ public class Request {
         this.name = name;
     }
 
-    public Request(String name) {
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public Request(String name, String image, int x, int y, int width, int height) {
         this.name = name;
+        this.image = image;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
     }
 
     public Request() {
-
     }
 }
