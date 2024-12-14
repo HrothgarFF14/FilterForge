32fea1ace25f4606da5236f9da57ec71c8feca74
Louis Lomboy <llomboy@uw.edu>
2024-12-13 23:42:28 -0800

    merge

--- python_functions/container_template/Run_Function.py
+++
@@ -1,4 +0,0 @@
-import FaaSET
-
-results = FaaSET.bash_container({})
-print(results["standard_output"])
--- python_functions/container_template/platforms/aws_docker/your_script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-json_data=$1
-
-name=$(echo "$json_data" | jq -r '.name')
-
-echo "{\"message\": \"Hello $name!\"}"
